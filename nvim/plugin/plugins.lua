6dce41123236b8bd3de2bf409c587602b6b8c557
David Shepard <daveman1010220@gmail.com>
2024-04-05 22:16:12 -0400

    Update plugins.lua

--- nvim/plugin/plugins.lua
+++ nvim/plugin/plugins.lua
@@ -6,5 +6,50 @@ vim.g.did_load_plugins_plugin = true
 -- many plugins annoyingly require a call to a 'setup' function to be loaded,
 -- even with default configs
 
+require('nvim-tree').setup({
+   sort_by = "case_sensitive",
+   view = {
+     width = 30,
+   },
+   renderer = {
+     group_empty = true,
+   },
+   filters = {
+     dotfiles = true,
+   },
+})
+
+require('gruvbox').setup({
+  terminal_colors = true, -- add neovim terminal colors
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    emphasis = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "hard", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = false,
+})
+
+configs.setup({
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "css", "meson", "python", "rust", "toml", "csv" },
+  sync_install = false,
+  highlight = { enable = true },
+  indent = { enable = true },
+})
+
 require('nvim-surround').setup()
 require('which-key').setup()
